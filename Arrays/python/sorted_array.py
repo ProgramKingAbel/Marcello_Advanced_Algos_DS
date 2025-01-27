7a3bb7c0381a7b7381ba9ee5c4a53d65f1c944b7
Abel Morara <abelmorara254@gmail.com>
2025-01-27 15:31:30 +0300

    feat: implement insertion sort on sorted static arr

---
+++ Arrays/python/sorted_array.py
@@ -0,0 +1,21 @@
+from core import Array
+
+class SortedArray():
+    def __init__(self, max_size, typecode='l'):
+        self._array = Array(max_size, typecode)
+        self._max_size = max_size
+        self._size = 0
+
+    def insert(self, value):
+        if self._size >= self._max_size:
+            raise ValueError(f'The array is already full, maximum size {self._max_size}')
+        
+        for i in range(self._size, 0, -1):
+            if self._array[i-1] <= value:
+                self._array[i] = value
+                self._size += 1
+            else:
+                self._array[i] = self._array[i-1]
+        
+        self._array[0] = value
+        self._size += 1
