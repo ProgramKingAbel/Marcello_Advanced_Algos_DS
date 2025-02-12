9e93adbbf7cd96785e975e4feeaef1066ea2144c
Abel Morara <abelmorara254@gmail.com>
2025-02-12 19:57:44 +0300

    implement sorted arrays

--- Arrays/python/sorted_array.py
+++ Arrays/python/sorted_array.py
@@ -19,3 +19,54 @@ class SortedArray():
         
         self._array[0] = value
         self._size += 1
+
+    def delete_by_index(self, target_index):
+        if target_index < 0 or target_index >= self._size:
+            raise ValueError(f'Index {target_index} is out of bounds')
+        
+        for i in range(target_index, self._size - 1):
+            self._array[i] = self._array[i + 1]
+        
+        self._size -= 1
+
+    def delete(self, target):
+        index = self.linear_search(target)
+
+        if index is None:
+            raise ValueError(f'Unable to delete alement {target}: the element is not in the array')
+        
+        for i in range(index, self._size - 1):
+            self._array[i] = self._array[i + 1]
+
+        self._size - 1
+    
+    def linear_search(self, target):
+        for i in range(0, self._size):
+            if self._array[i] == target:
+                return i
+            
+            elif self._array[i] > target:
+                return None
+        return None
+    
+    def binary_search(self, target):
+        left = 0
+        right = self._size - 1
+
+        while left <= right:
+            mid_index = (left + right) // 2
+            mid_val = self._array[mid_index]
+
+            if mid_val == target:
+                return mid_index
+            
+            elif mid_val > target:
+                right = mid_index - 1
+
+            else:
+                left = mid_index + 1
+        return None
+    
+    def traverse(self, callback):
+        for i in range(0, self._size):
+            callback(self._array[i])
